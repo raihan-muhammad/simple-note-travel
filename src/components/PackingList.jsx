7ec8167af867a72b0051d68241944b15a7b10901
raihan-muhammad <sukasukangoding@gmail.com>
2024-01-11 20:59:23 +0700

    feat: add sort list function

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,16 +1,38 @@
+import { useState } from "react";
+
 export default function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div className="list">
       <ul>
-        {items.map((item, i) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
-            key={i}
+            key={item.id}
             onDeleteItem={onDeleteItem}
             onToggleItem={onToggleItem}
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
@@ -23,7 +45,7 @@ function Item({ item, onDeleteItem, onToggleItem }) {
         value={item.packed}
         onChange={() => onToggleItem(item.id)}
       />
-      <span style={item.packed ? { textDecorationLine: "line-through" } : {}}>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
       <button onClick={() => onDeleteItem(item.id)}>❌</button>
